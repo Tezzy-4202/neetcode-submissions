ba454adbcd74db36b02828d7654b6a3a6b7775cf
Arun Teja <97935516+Tezzy-4202@users.noreply.github.com>
2026-08-15 11:52:50 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.cpp
@@ -0,0 +1,12 @@
+#include <cstring>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        if (s.length()!=t.length()) return false;
+        return s==t;
+    }
+};
